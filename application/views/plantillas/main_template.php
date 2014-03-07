0a3633c83b324a7a41248c1e47657030dad4e94c
root <root@john-HP-G42-Notebook-PC.(none)>
2014-03-06 23:48:43 -0500

    main

---
+++ application/views/plantillas/main_template.php
@@ -0,0 +1,9 @@
+<?php
+
+	$data['title']= $title;
+	$this->load->view('plantillas/header',$data);
+	$this->load->view('plantillas/navegacion');
+	$this->load->view($main_content);
+	$this->load->view('plantillas/footer');
+	
+	
