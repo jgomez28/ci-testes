8aa78816f86835b882f7b63809ac2b2ee5928098
Oscar Sobrevilla <oscar.sobrevilla@gmail.com>
2014-03-07 00:14:32 -0500

    correccion de errores de sintaxis y activacion de mostrado de errores

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -1,26 +1,31 @@
 <?php
 
-class blog extends CI_Controller {
+class Blog extends CI_Controller {
+
+	public function index(){
+		$this->inicio();
+	}
 
 	public function inicio(){
 			$data['title']='Inicio';
 			$data['main_content']='inicio';
-			$this->load->view('includes/main_template',$data);
+			$this->load->view('plantillas/main_template', $data);
 	}
 	public function acercade(){
 			$data['title']='Acerca de';
 			$data['main_content']='acercade';
-			$this->load->view('plantillas/main_template'$data);
+			$this->load->view('plantillas/main_template', $data);
 	}	
 	public function servicios(){
 			$data['title']='Servicios';
 			$data['main_content']='servicios';
-			$this->load->view('plantillas/main_template'$data);
+			$this->load->view('plantillas/main_template', $data);
 	}
 	public function contacto(){
 			$data['title']='Contacto';
 			$data['main_content']='contacto';
-			$this->load->view('plantillas/main_template'$data);
+			$this->load->view('plantillas/main_template', $data);
+	}
 
 }
 	
